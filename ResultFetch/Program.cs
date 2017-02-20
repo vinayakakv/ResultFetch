00d14e9f3ecb0ea949d7782366bec35f8970569f
Vinayaka K V <vinayakakv@live.in>
2017-02-20 23:59:08 +0530

    First Commit

---
+++ ResultFetch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+
+namespace ResultFetch
+{
+    class Program
+    {
+        static async Task<string> FetchResult()
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    ["USN"] = "4JC15CS129",
+                    ["Action"] = "Fetch+Result"
+                };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("http://sjce.ac.in/view-results", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                return responseString;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() => Console.WriteLine(FetchResult().Result));
+            t.Start();
+            for (int i = 0; i < 100; i++)
+                Console.Write("*");
+            t.Join();
+            Console.ReadKey();
+        }
+    }
+}
